f9e48cc2704983c206ba1bb9cb86dbedef85af96
Praveen <praveenkumarkalla@gmail.com>
2025-06-04 15:57:14 -0400

    Praveen - Commit

--- src/main/java/com/task/agentic/controller/TasksController.java
+++ src/main/java/com/task/agentic/controller/TasksController.java
@@ -1,34 +1,31 @@
 package com.task.agentic.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.task.agentic.model.TaskDetailsDTO;
 import com.task.agentic.services.TaskProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/api/taskExecutor")
 public class TasksController {
 
+    @Autowired
     TaskProcessor taskProcessor;
 
-    @GetMapping(consumes = MediaType.ALL_VALUE)
-    public ResponseEntity<String> taskExecutor(){
+    @PostMapping(consumes = MediaType.ALL_VALUE, produces = MediaType.ALL_VALUE)
+    public ResponseEntity<String> taskExecutor(@RequestBody @NotNull TaskDetailsDTO taskDetails) throws JsonProcessingException {
 
-        String certTask = taskDetails();
-        return new ResponseEntity<>(taskProcessor.process(certTask), HttpStatus.OK);
+        taskProcessor.process(taskDetails);
+        return new ResponseEntity<>("Executed..!", HttpStatus.OK);
     }
-
-    public static String taskDetails(){
-
-        return "[{\n" +
-                "\t\"taskId\" : \"abcd1234\",\n" +
-                "\t\"adGroupId\" : \"ADGRP12345\",\n" +
-                "\t\"brid\" : \"G01253654\"\n" +
-                " }]";
-    }
-
 
 }
--- src/main/java/com/task/agentic/services/TaskProcessorImpl.java
+++ src/main/java/com/task/agentic/services/TaskProcessorImpl.java
@@ -1,13 +1,15 @@
 package com.task.agentic.services;
 
+import com.task.agentic.model.TaskDetailsDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskProcessorImpl implements TaskProcessor {
 
     @Override
-    public String process(String taskInfo){
-        return taskInfo;
+    public String process(TaskDetailsDTO taskInfo){
+        System.out.println("Task Input : " + taskInfo);
+        return taskInfo.toString();
     }
 
 
